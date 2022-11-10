2f6fb7d151e2cb2a355328fba0e0041c56319058
K5709 <r.kristian40@gmail.com>
2022-11-10 15:22:54 -0600

    sort of met mvp, need to finish

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import Form from './form';
 
 const playersList = [
-  {playerName: 'Justin Fields', position: 'Quarter Back', age: '22', },
-  {playerName: 'Darnell Mooney', position: 'Wide Receiver', age: '25'},
-  {playerName: 'David Montgomery', position: 'Running Back', age: '25'},
-  {playerName: 'Chase Claypool', position: 'Wide Receiver', age: '24'},
-  {playerName: 'Eddie Jackson', position: 'Safety', age: '28'}
+  { playerName: 'Justin Fields', position: 'Quarter Back', age: '22', },
+  { playerName: 'Darnell Mooney', position: 'Wide Receiver', age: '25' },
+  { playerName: 'David Montgomery', position: 'Running Back', age: '25' },
+  { playerName: 'Chase Claypool', position: 'Wide Receiver', age: '24' },
+  { playerName: 'Eddie Jackson', position: 'Safety', age: '28' }
 ]
 
 const initialFormValues = {
-  username: '',
+  playerName: '',
   position: '',
   age: ''
 }
@@ -20,23 +20,42 @@ function App() {
   const [players, setPlayers] = useState(playersList);
   const [formValues, setFormValues] = useState(initialFormValues)
 
+  const change = (evt) => {
+    const { name, value } = evt.target
+    setFormValues({
+      ...formValues,
+      [name]: value}
+      );
+  }
+  const submit = (evt) => {
+    evt.preventDefault()
+    const newPlayer = {
+      playerName: formValues.playerName.trim(),
+      position: formValues.position.trim(),
+      age: formValues.age.trim()
+  }
+  setPlayers(players.concat(newPlayer));
+    setFormValues(initialFormValues);
+  }
 
-
-  return(
+  return (
     <div className='App'>
-      <h1>Da Bears</h1>
-      { playersList.map((players, idx) =>{
-      return(
-        <div key={idx}>
-          <p>Player Name: {players.playerName}</p>
-          <p>Position: {players.position}</p>
-          <p>Age: {players.age}</p>
-          <Form />
-
+      <img className='bearslogo' src='https://images.thdstatic.com/productImages/a1087a37-4b67-4e79-beaf-2eb162e208a4/svn/navy-fanmats-sports-rugs-17954-64_400.jpg ' alt='picture of the bears logo' />
+      <h1>Da' Bears</h1>
+      {playersList.map((players, idx) => {
+        return (
+          <div key={idx}>
+            <p>Player Name: {players.playerName}</p>
+            <p>Position: {players.position}</p>
+            <p>Age: {players.age}</p>
           </div>
-          
-      )
+        )
       })}
+      <div className='Form'>
+        <Form change={change}
+         submit={submit}
+          formValues={formValues}/>
+      </div>
     </div>
   )
 }
--- src/form.js
+++ src/form.js
@@ -1,32 +1,53 @@
 import React from 'react'
-import App from './App';
-function Form() {
+import './App.css';
 
-    const change = (evt) => {
-        const {name, value} =evt.target;
-        
-    }
 
-    return(
-    <form>
-        <input
-        name='playerName'
-        type='text'
-        value=''
-        onChange={ change }
-        />
-        <input 
-        position='position'
-        type='text'
-        value=''
-        onChange={ change }
-        />
-        <input 
-        age='age'
-        type='text'
-        value=''
-        onChange={ change }
-        />
+function Form(props) {
+
+    const {
+        change,
+        formValues,
+        submit
+    } = props
+
+    return (
+        <form onSubmit={submit}>
+            <h2>Got what it takes? Create a player here...</h2>
+            <label>Player Name:
+            <input
+                placeholder='Insert Name here..'
+                name='playerName'
+                type='text'
+                value={formValues.playerName}
+                onChange={change}
+            />
+            </label>
+            <br />
+            <label>Position:
+            <select value={formValues.role}
+             className='positionSelection'
+            onChange={change}>
+
+                <option value=''>Select Position...</option>
+                <option value='qb'>Quarter Back</option>
+                <option value='wr'>Wide Receiver</option>
+                <option value='rb'>Running Back</option>
+                <option value='s'>Safety</option>
+                <option value='other'>Other</option>
+            </select>
+            </label>
+            <br />
+            <label>Age:
+            <input
+                placeholder='Select age'
+                name='age'
+                type='number'
+                value={formValues.age}
+                onChange={change}
+            />
+            </label>
+            <br />
+            <button onSubmit={submit}> Submit ya chump! </button>
         </form>
     )
 }
